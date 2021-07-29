97103e5f5d67d16f02a7311e50ed3ea2172785fe
Kevin Sjöberg <mail@kevinsjoberg.com>
2021-07-29 20:56:34 +0200

    Add minimal Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  mode: 'development'
+}
